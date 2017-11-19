faf911633b505703f4e5608e79c798f584a4add3
Audrey Strelzoff <astrelzoff@ufl.edu>
2017-11-19 13:57:04 -0600

    Pass a css object with 3 pairs to the paragraphs variable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
     //Assign a variable called paragraphs equal to all the p tags in the documentElement
     var paragraphs = $('p');
+    $(paragraphs).css({'color': 'maroon', 'background-color': 'lavender', 'line-height': '1.5rem'});
 });
